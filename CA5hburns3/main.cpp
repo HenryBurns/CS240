a974c4e8081e7c0fe1dd6e08ab34c65e18d9541f
Henry Burns <hburns3@cs.binghamton.edu>
2018-12-04 06:19:53 -0500

    PART 1 WORKZ

--- CA5hburns3/main.cpp
+++ CA5hburns3/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <string>
 #include <unordered_map>
+#include <queue>
 #include "node.hpp"
 
 int main(int argc, char* argv[]){
@@ -16,12 +18,122 @@ int main(int argc, char* argv[]){
     Node* temp_node;
     Flight* temp_flight;
     while(file >> departure_city >> arrival_city >> arrival_time >> departure_time){
-        if(getter.find(departure_city) == getter.end()){
+        std::cout << "Departure: " << departure_city << ". Arrival city: " << arrival_city << std::endl;
+        if(getter[departure_city] == NULL){
             temp_node = new Node(departure_city); 
             getter[departure_city] = temp_node;
         } 
-        temp_flight = new Flight(arrival_time, departure_time, 0);
+        if(getter[arrival_city] == NULL){
+            temp_node = new Node(arrival_city); 
+            getter[arrival_city] = temp_node;
+        } temp_flight = new Flight(arrival_time, departure_time, 0, arrival_city);
+        temp_flight->destination_node = getter[arrival_city];
         getter[departure_city]->flights.push_back(temp_flight);
+        
+    }
+    std::cout << "Enter a departure city." << std::endl;
+    std::cin >> departure_city;
+    departure_city.erase(std::remove(departure_city.begin(), departure_city.end(), 'n'), departure_city.end());
+    std::cout << "Enter a destination city." << std::endl;
+    std::cin >> arrival_city;
+    arrival_city.erase(std::remove(arrival_city.begin(), arrival_city.end(), 'n'), arrival_city.end());
+    std::cout << "Enter the earliest acceptable departure time." << std::endl;
+    std::cin >> departure_time;
+    unsigned int dept_time = setTime(departure_time);
+    std::string inp;
+    std::string any = "any";
+    std::string Earliest = "earliest";
+    std::string Least = "cheapest";
+    std::queue<Node*> frontier;
+    frontier.push(getter[departure_city]);
+    Node* temp  = getter[departure_city];
+    std::cout << "temp test " << temp->Name << std::endl;
+    Node* temp2 = getter[arrival_city];
+    auto end = temp;
+    auto start = temp2;
+    std::cout << "Temp Before : " <<temp << std::endl;
+    while(true){
+        std::cout << "Type either 'any', 'earliest', or 'cheapest'" << std::endl;
+        std::cin >> inp;
+        if(inp.compare(any) == 0){
+            std::cout << "Entered any" << std::endl;
 
+            while(!frontier.empty()){
+                temp = frontier.front();
+                std::cout << "TEMP: " << temp->Name << std::endl;
+                temp->visited = 2;
+                frontier.pop();
+                for(int i = 0; i < temp->flights.size(); i++){
+                    Node* k = getter[temp->flights[i]->destination];
+                    if(k != NULL){
+                        Node* iter = k; 
+                        std::cout << "Iter: " << iter->Name << std::endl;
+                        //Node iter = *((*i)->destination_node);
+                        if(iter->visited < 2 && temp->flights[i]->departure >= dept_time){
+                            iter->visited = 1;
+                            k->prev = temp;
+                            if(iter->prev != temp)
+                                std::cout << "Temp prev: " << iter->prev->Name << std::endl;
+                            //update everything
+                            //if(iter->arrival == 0)
+                                //iter->arrival = temp->flights[i]->arrival;
+                            //else
+                            //    iter->arrival = std::min(iter->arrival, temp->flights[i]->arrival);
+
+                            //if(iter->cost == 0){
+                            //    iter->cost = iter->prev->cost + temp->flights[i]->cost;
+                            //    std::cout << "Prev cost name " << iter->prev->Name << std::endl;
+                            //}
+                            //else
+                            //    iter->cost = std::min(iter->cost, iter->prev->cost + temp->flights[i]->cost);
+                            //check if we hit the end
+                            if(iter->Name.compare(arrival_city) == 0){
+                                iter->prev = temp;
+                                std::cout << "DONE" << " Past: " << iter->prev->Name << std::endl;
+                                //std::cout << "2 past " << iter->prev->prev->Name << std::endl;
+                                while(!frontier.empty())
+                                    frontier.pop();
+                                break;
+                            }
+                            frontier.push(k);
+                        }
+                    }
+                    else
+                        std::cout << "WHY IS THIS NULL??? " << std::endl;
+                }
+            }
+            std::cout << "Arrival city: " << arrival_city << std::endl;
+            std::cout << "Departing city: " << departure_city << std::endl;
+            temp = end;
+            std::cout << "Temp: " << temp->Name << std::endl;
+            temp2 = getter[arrival_city];
+            while(temp != temp2){
+//                if(temp == NULL){
+//                    std::cout << "This node cannot be reached" << std::endl;
+//                    break;
+                std::cout << "Tracing Back: " << temp2->Name << std::endl;
+//                }
+//                else {std::cout << "Temp is Null" << std::endl; }
+                temp2 = temp2->prev;
+            }
+            std::cout << "Tracing Back LAST: " << temp2->Name << std::endl;
+            break;
+        }
+        else if(inp.compare(Earliest)){
+            //TODO find the earliest path
+        }
+        else if(inp.compare(Least)){
+            //TODO get the cheapest path
+        }
+        else{
+            std::cout << "Input was not formatted correctly." << std::endl;
+        }
+    }
+    for(auto i = getter.begin(); i != getter.end(); ++i){
+        if(i->second != NULL){
+            for(auto j = i->second->flights.begin(); j != i->second->flights.end(); ++j)
+                delete (*j);
+        }
+        delete i->second;
     }
 }
--- CA5hburns3/node.hpp
+++ CA5hburns3/node.hpp
@@ -5,44 +5,44 @@
 #include <iostream>
 class Node;
 
-int setTime(std::string Time_val){
-            std::cout << "Input Time: " << Time_val << std::endl;
+unsigned int setTime(std::string Time_val){
             const char* temp = Time_val.c_str();
             char m = '\0';
-            int hour = 0;
-            int min = 0;
-            std::cout << "Starting scan" << std::endl;
+            unsigned int hour = 0;
+            unsigned int min = 0;
             printf("%s\n", temp);
             sscanf(temp, "%u:%u%cm", &hour, &min, &m);
-            std::cout << "Hour: " << hour << std::endl;
-            std::cout << "Min: " << min << std::endl;
             if(m == 'p')
                 hour += 12;
             if((hour %12) == 0)
                 hour -= 12;
-            int val = (hour * 100) + min;
-            std::cout << "Returning int: " << val << std::endl;
+            unsigned int val = (hour * 100) + min;
             return val;
         }
 class Flight{
     public:
         std::string destination = "[empty]";
-        Node* destination_node;
-        unsigned int cost = 0;
+        Node* destination_node = NULL;
+        float cost = 0;
         unsigned int arrival = 0 ;
         unsigned int departure = 0;
         Flight(){}
-        Flight(std::string start, std::string end, int mon){
+        Flight(std::string start, std::string end, int mon, std::string dest){
             arrival = setTime(start);
             departure = setTime(end);
             cost = mon;
+            destination = dest;
         }
 };
 
 class Node{
     public:
+        Node* prev = NULL;
         std::string Name = "[empty]";
         std::vector<Flight*> flights;
+        unsigned int arrival = 0;
+        float cost = 0;
+        unsigned int visited = 0;
         Node(std::string str){
             Name = str;
         }
